b5998f68cb818f57938591653fd14a7557ff0478
shishandri <shishandrikaul9@gmail.com>
2021-04-01 11:23:46 +0530

    'dailyupdate'

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -10,13 +10,18 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  gender: any = ['Male', 'FeMale']
   userDetails:any;
   abc:any;
+  web:any;
+  profilepic:any;
+  add:any;
+  mob:any;
   fileUploadForm: any;
   fileInputLabel: any;
   url:any;
   urlimg:String='https://i.ibb.co/fDWsn3G/buck.jpg';
-  form:any;
+  alternative:any;
   employeeForm: any; 
  
   constructor(private userService: UserService,private formBuilder: FormBuilder, private router: Router, private http: HttpClient) { }
@@ -25,6 +30,7 @@ export class UserProfileComponent implements OnInit {
     debugger;
    this.fileUploadForm = this.formBuilder.group({
     address: [""],
+    gender: [""],
     mobile:[''],
     alternativeNo:[''],
     uploadedImage: ['']
@@ -42,6 +48,10 @@ export class UserProfileComponent implements OnInit {
     );
    
     this.getprofile();
+  }
+  changeWebsite(e) {
+    console.log(e.target.value);
+    alert(e.target.value)
   }
 
   onFileSelect(event:any) {
@@ -69,26 +79,49 @@ export class UserProfileComponent implements OnInit {
   
   onFormSubmit() {
      debugger;
-       
-     
-        const formData = new FormData();
+  
+  var first_json = this.fileUploadForm.value;
+  var second_json = this.userDetails;
+  var jsons = new Array();
+  jsons.push(first_json);
+  jsons.push(second_json);
+   
+      
+ if(this.fileUploadForm.value.uploadedImage=="")
+ {
+  this.http
+  .post<any>('http://localhost:3000/api/updateUserProfilepic',jsons).subscribe(response => {
+    debugger;
+    console.log(response);
+   const urlimg=response['uploadedImage'];
+  
+  });
+}
+  else{
+  const formData = new FormData();
         formData.append('address', this.fileUploadForm.get('address').value);
+        formData.append('uploadedImage', this.fileUploadForm.get('uploadedImage').value);
+        formData.append('gender', this.fileUploadForm.get('gender').value);
         formData.append('mobile', this.fileUploadForm.get('mobile').value);
         formData.append('_id', this.userDetails._id);
         formData.append('alternativeNo', this.fileUploadForm.get('alternativeNo').value);
-        formData.append('uploadedImage', this.fileUploadForm.get('uploadedImage').value);
         formData.append('agentId', '007');
-    
-         debugger;
-        this.http
-          .post<any>('http://localhost:3000/api/updateUserProfile',formData).subscribe(response => {
-            debugger;
-            console.log(response);
-           const urlimg=response['uploadedImage'];
-          
-          });
+    this.http
+    .post<any>('http://localhost:3000/api/updateUserProfile',formData).subscribe(response => {
+      debugger;
+     console.log(response);
+     const urlimg=response['uploadedImage'];
+    });
+  }
+  }
+      
          
-      }
+      
+      // uploadprofile()
+      // {
+      //    debugger;
+        
+      // }
     
 getprofile()
 {
@@ -96,16 +129,17 @@ getprofile()
   this.userService.getUserProfile1().subscribe((res:any) => {
       debugger;
       this.url=res['userPro'];
-      
+      this.add= this.url.address;
+      this.mob= this.url.mobile;
+      this.alternative= this.url.alternativeNo;
       this.urlimg= this.url.uploadedImage;
-  
-    //   this.url.mobile;
-    // this.url.alternativeNo;
-      // console.log("hello");
+    this.web= this.url.gender;
+ 
     },
    
   );
 }
+
   onLogout(){
     this.userService.deleteToken();
     this.router.navigate(['/login']);
